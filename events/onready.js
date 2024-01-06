8faef1125f9d6d05ad22d2ef24265bb2530c0ddd
BlazeGamesYT <bryceblazegaming@gmail.com>
2024-01-05 23:15:06 -0800

    Added shuffling statuses with configurable json file :3

--- events/onready.js
+++ events/onready.js
@@ -1,14 +1,20 @@
 const { Events, ActivityType } = require("discord.js");
+const activities = require("../config/activities.json");
+
+const statusIntervalSecs = 10;
 
 module.exports = {
     name: Events.ClientReady,
     async listener(client) {
-        let activity = {
-            name: "femboys be femboys",
-            type: ActivityType.Watching
-        };
+        client.user.setActivity({
+            type: ActivityType.Playing,
+            name: "booting up..."
+        });
 
-        client.user.setActivity(activity);
+        setInterval(() => {
+            const index = Math.floor(Math.random() * activities.length);
+            client.user.setActivity(activities[index]);
+        }, statusIntervalSecs * 1000);
 
         console.log("Bot started!");
     }
